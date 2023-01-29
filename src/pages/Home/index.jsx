84189463746337fad1a663ba4e571887a4219b45
ys-0323 <dingjb@flamelephant.com>
2023-01-29 14:12:02 +0800

    add: 引用sass文件

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
+import { useStore } from '@/store'
+import styles from './index.module.scss'
 
 function HomePage(){
+    const { login } = useStore()
     return (
         <>
+        <div className={styles.loginName}>
+             欢迎 { login.mobile || '游客1234'}
+        </div>
         首页
         </>
     )
